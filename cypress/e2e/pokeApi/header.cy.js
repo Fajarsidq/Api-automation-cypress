51e01bf4c936c3c700c8f7451d222134c8ece778
Fajarsidq <140775958+Fajarsidq@users.noreply.github.com>
2024-04-06 20:50:01 +0700

    update validate

--- cypress/e2e/pokeApi/header.cy.js
+++ cypress/e2e/pokeApi/header.cy.js
@@ -1,4 +1,6 @@
-describe('Validate Header', () => {
+/// <reference types="cypress" />
+
+describe('Validate api', () => {
 
     it('Succesfully validate  content-type', () => {
         cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
@@ -15,8 +17,34 @@ describe('Validate Header', () => {
             { name: 'ditto', url: 'https://pokeapi.co/api/v2/pokemon-form/132/' }
             ],
         });
-    
+    })
+        it('Succesfully validate  status code', () => {
+            cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('ditto')
+            cy.get('@ditto').its('status').should('equal',200)
+
+        })
+
+        it('Succesfully validate ststus code with params', () => {
+            cy.request({
+                method: 'GET',
+                url: 'https://reqres.in/api/users'
+            }).as('users')
+            cy.get('@users').its('status').should('equal',200)
+
+        })
 
         
-})
+        it('Succesfully validate  content', () => {
+            cy.request('https://pokeapi.co/api/v2/pokemon/bulbasaur').as('bulbasaur')
+            cy.get('@bulbasaur').its('body').should('include',{name: "bulbasaur"})
+
+
+        })
+        it('Succesfully validate  content', () => {
+            cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('ditto1')
+            cy.get('@ditto1').its('body').should('include',{name: "bulbasaur"})
+
+
+        })
+
 })
